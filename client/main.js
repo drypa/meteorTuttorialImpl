2b0e1c14abbeba2a61c099522ad1b5278c1c47b8
DRypa <dr.drypa.k@mail.ru>
2016-08-30 08:17:04 +0300

    user accounts added +meteor version updated

--- client/main.js
+++ client/main.js
@@ -1,9 +1,10 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import todoList from '../imports/components/todoList/todoList'
-
+import '../imports/startup/accounts-config'
 
 angular.module('simple-todos',[
   angularMeteor,
-  todoList.name
+  todoList.name,
+  'accounts.ui'
 ]);
